5bcfdbf3842613cd33573e3f8091308bc61e8c28
ryanchangx <ryanchangx@gmail.com>
2021-02-27 00:22:01 -0800

    Push progress in visitor

--- visitor.hpp
+++ visitor.hpp
@@ -1,6 +1,7 @@
 #ifndef __VISITOR_HPP__
 #define __VISITOR_HPP__
 
+#include <iostream>
 class Op;
 class Rand;
 class Add;
@@ -36,6 +37,27 @@ class Visitor{
         virtual void visit_pow_begin(Pow* node) = 0;
         virtual void visit_pow_middle(Pow* node) = 0;
         virtual void visit_pow_end(Pow* node) = 0;
+};
+
+class VisitorLatex : public Visitor{
+	public:
+        virtual void visit_op(Op* node);
+        virtual void visit_rand(Rand* node);
+        virtual void visit_add_begin(Add* node);
+        virtual void visit_add_middle(Add* node);
+        virtual void visit_add_end(Add* node);
+        virtual void visit_sub_begin(Sub* node);
+        virtual void visit_sub_middle(Sub* node);
+        virtual void visit_sub_end(Sub* node);
+        virtual void visit_mult_begin(Mult* node);
+        virtual void visit_mult_middle(Mult* node);
+        virtual void visit_mult_end(Mult* node);
+        virtual void visit_div_begin(Div* node);
+        virtual void visit_div_middle(Div* node);
+        virtual void visit_div_end(Div* node);
+        virtual void visit_pow_begin(Pow* node);
+        virtual void visit_pow_middle(Pow* node);
+        virtual void visit_pow_end(Pow* node);
 };
 
 #endif //__VISITOR_HPP__
